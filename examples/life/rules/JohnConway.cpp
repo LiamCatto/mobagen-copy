43154a89912a256565d8d8f2244e7b457a8c8dfa
LiamCatto <liam.mercado@mymail.champlain.edu>
2024-09-13 14:06:51 -0400

    Added notes to game of life assignment

--- examples/life/rules/JohnConway.cpp
+++ examples/life/rules/JohnConway.cpp
@@ -3,9 +3,17 @@
 // Reference: https://playgameoflife.com/info
 void JohnConway::Step(World& world) {
   // todo: implement
+
 }
 
 int JohnConway::CountNeighbors(World& world, Point2D point) {
   // todo: implement
+
+
+
   return 0;
 }
+
+
+// Note to self: step represents a change in world state -- iterate through each cell once.
+// buffer holds an entire world state. Will need to set the buffer.
