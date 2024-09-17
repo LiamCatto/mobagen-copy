2900ff3dbbebbbe406dafd1ee475d5dabd29bd30
LiamCatto <liam.mercado@mymail.champlain.edu>
2024-09-17 14:10:13 -0400

    Began fixing an issue with CountNeighbors
    Unsure if the issue is completely fixed. Before it was only considering the top left neighbor, but it should be considering all neighbors surrounding the point.

--- examples/life/rules/JohnConway.cpp
+++ examples/life/rules/JohnConway.cpp
@@ -4,43 +4,42 @@
 // Reference: https://playgameoflife.com/info
 void JohnConway::Step(World& world) {
   // todo: implement
+
   Point2D currentPoint(0, 0);
   bool currentState = true;   // The state of the current point
   bool finalState  = true;    // The final state of the current point
 
   for (int i = 0; i < world.SideSize(); i++) {
     for (int k = 0; k < world.SideSize(); k++) {
-      currentPoint = Point2D(i, k);
+      currentPoint = Point2D(k, i);
       currentState = world.Get(currentPoint);
 
       // Check rules for live cells
       if (currentState == 1) {
-        // Check for underpopulation
+        // // Check for underpopulation
         if (CountNeighbors(world, currentPoint) < 2) {
           finalState = 0;
-        }
+        } // Bug: All previous cells are suddenly set to alive
 
         // Check if the cell will live on
         if (CountNeighbors(world, currentPoint) == 2 || CountNeighbors(world, currentPoint) == 3) {
           finalState = 1;
-        }
+        } // Bug: All cells are suddenly set to alive
 
         // Check for overpopulation
         if (CountNeighbors(world, currentPoint) > 3) {
           finalState = 0;
-        }
+        } // Bug: All cells are suddenly set to alive
       } else {  // Check rules for dead cells
         // Check for reproduction
         if (CountNeighbors(world, currentPoint) == 2) {
           finalState = 1;
-        }
+        } // Bug: All cells are suddenly set to alive
       }
 
       world.SetNext(currentPoint, finalState);
     }
   }
-
-  world.SwapBuffers();
 }
 
 int JohnConway::CountNeighbors(World& world, Point2D point) {
@@ -49,14 +48,16 @@ int JohnConway::CountNeighbors(World& world, Point2D point) {
   int liveNeighbors = 0;
   Point2D topLeftAdjacent (point.x - 1, point.y - 1);   // The cell up and to the left of the point
 
-  for (int i = topLeftAdjacent.y; i < point.y + 1; i++) {
-    for (int k = topLeftAdjacent.x; k < point.x + 1; k++) {
-      if (world.Get(Point2D(k, i)) == true && k != point.x && i != point.y) {
+  for (int i = topLeftAdjacent.y; i < point.y + 2; i++) {
+    for (int k = topLeftAdjacent.x; k < point.x + 2; k++) {
+      if (world.Get(Point2D(k, i)) == true && Point2D(k, i) != point) {
         liveNeighbors++;
+        std::cout << 1;
       }
     }
   }
-  //std::cout << liveNeighbors << std::endl;
+  std::cout << std::endl;
+  std::cout << liveNeighbors << ", " << point.x << ", " << point.y << std::endl;
   return liveNeighbors;
 }
 
